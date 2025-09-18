f66f3680e340ad0aac39c59744971e9a0a858325
antonioeduthz <antonioedu.thz@gmail.com>
2025-09-18 02:36:20 -0300

    Use-jquery

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const notes = JSON.parse(localStorage.getItem("notes") || "[]")
 
 
@@ -114,4 +115,122 @@ deleteButton.addEventListener('click', () => {
     clearDisplay();
 });
 
+=======
+const notes = JSON.parse(localStorage.getItem("notes") || "[]")
+
+
+//Elementos do display
+const display = document.querySelector("#display");
+const noteTitle = document.querySelector("#note-title");
+const noteContent = document.querySelector("#note-content");
+const saveButton = document.querySelector("#save-btn");
+const deleteButton = document.querySelector("#delete-btn");
+let isDisplayOn = false;
+let isNoteOn = false;
+let noteIndex;
+
+//Misc
+const main = document.querySelector("main");
+const template = document.getElementById("note-template")
+const addButton = document.querySelector("#add-btn");
+const noteMain = document.getElementById("note-main");
+
+//funções para salvar, atualizar e deletar
+function SaveData(title, content)
+{
+    notes.push([title,content]);
+    updateNotes();
+    localStorage.setItem("notes",JSON.stringify(notes));
+}
+function updateData()
+{
+    notes[noteIndex][0] = noteTitle.value;
+    notes[noteIndex][1] = noteContent.value;
+    updateNotes();
+    localStorage.setItem("notes",JSON.stringify(notes));
+}
+function deleteData()
+{
+    notes.splice(noteIndex,1);
+    updateNotes();
+    localStorage.setItem("notes",JSON.stringify(notes));    
+}
+
+//Atualiza a lista de notas 
+function updateNotes()
+{
+    noteMain.innerHTML = "";
+    notes.forEach((element,index) => {
+        let clone = template.content.cloneNode(true);
+        const previewNote = clone.querySelector(".note-preview")
+        const fade = clone.querySelector(".fade");
+        previewNote.setAttribute("id", index);
+        clone.querySelector(".title-preview").textContent = element[0];
+        clone.querySelector(".p-preview").textContent = element[1];
+        previewNote.addEventListener('click', () => {
+            noteTitle.value = element[0];
+            noteContent.value = element[1];
+            deleteButton.textContent = "Delete";
+            display.style.scale = 1;
+            isDisplayOn = true;
+            isNoteOn = true;
+            noteIndex = previewNote.id;
+        });
+        noteMain.append(clone);
+        if (previewNote.scrollHeight < 250) {
+            fade.style.scale = 0;
+        }
+    });
+}
+
+//Limpa o display da nota
+function clearDisplay()
+{
+    noteTitle.value = "";
+    noteContent.value = "";
+}
+
+//Abre o display para criar notas
+addButton.addEventListener('click', () => {
+    if (!isDisplayOn) {
+        display.style.scale = 1;
+        isDisplayOn = true;
+    } else {
+        display.style.scale = 0;
+        isDisplayOn = false;
+        isNoteOn = false;
+    }
+    deleteButton.textContent = "Close";
+    clearDisplay();
+});
+
+//Salva notas
+saveButton.addEventListener('click', () => {
+    const title = noteTitle.value;
+    const content = noteContent.value;
+    if (isNoteOn) {
+        updateData()
+    }
+    else {
+        SaveData(title, content); 
+    }
+    display.style.scale = 0;
+    deleteButton.textContent = "Close";
+    isDisplayOn = false;
+    isNoteOn = false;
+    clearDisplay();
+});
+
+//Deleta a nota ou fecha o display(caso for criar uma nota)
+deleteButton.addEventListener('click', () => {
+    if (isNoteOn) {
+        deleteData();
+    }
+    display.style.scale = 0;
+    isDisplayOn = false;
+    isNoteOn = false;
+    clearDisplay();
+});
+
+>>>>>>> 13d5506b85091bbf4fe7d1410eff92c041c1aaf4
 updateNotes();
--- src/mainJquery.js
+++ src/mainJquery.js
@@ -1,3 +1,69 @@
+<<<<<<< HEAD
+$(function() {
+    const notes = JSON.parse(localStorage.getItem("notes") || "[]")
+    let isNoteOn = false
+    let currentNote = 0
+    
+    //Botao para criar uma nova note ou salvar uma já existente
+    $("#save-btn").on("click", () => {
+        console.log(currentNote)
+        const title = $("#display-title").val()
+        const content = $("#display-content").val()
+        if (isNoteOn) {
+            notes[currentNote].title = title
+            notes[currentNote].content = content
+        } else {
+            notes.push({title,content})
+        }
+        $("#display").toggleClass("hide-display")
+        localStorage.setItem("notes",JSON.stringify(notes))
+        updateNoteList()
+    })
+
+    $("#delete-btn").on("click", () => {
+        if (isNoteOn) {
+            notes.splice(currentNote,1)
+            localStorage.setItem("notes",JSON.stringify(notes))   
+        }
+        $("#display").toggleClass("hide-display")
+        updateNoteList()
+    })
+
+    $("#add-btn").on("click", () => {
+        $("#display").toggleClass("hide-display")
+        isNoteOn = false
+    })
+
+    //
+    function updateNoteList() {
+        const template = document.querySelector("#note-template")
+        const $wrapper = $('<div/>')
+        $("#note-main").empty()
+        notes.forEach((element, index) => {
+            const clone = template.content.cloneNode(true)
+            const $note = $(clone).find(".note")
+            $note.find(".note-title").text(element.title)
+            $note.find(".note-content").text(element.content)
+            $note.attr("id", index)
+            $note.on("click", function() {
+                const $display = $("#display")
+                $display.toggleClass("hide-display")
+                $display.find("#display-title").text(element.title)
+                $display.find("#display-content").text(element.content)
+
+                currentNote = $(this).attr("id")
+                console.log(currentNote)
+
+                isNoteOn = true
+            })
+
+            $wrapper.append(clone)
+        });
+        $("#note-main").append($wrapper.children())
+    }
+
+    updateNoteList()
+=======
 $(function() {
     const notes = JSON.parse(localStorage.getItem("notes") || "[]")
     let isNoteOn = false
@@ -36,4 +102,5 @@ $(function() {
         });
         $("#note-main").append($frag)
     }
+>>>>>>> 13d5506b85091bbf4fe7d1410eff92c041c1aaf4
 })
