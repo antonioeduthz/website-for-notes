8a6d3519bc064a3efa1c315033afb9417b98f6f1
antonioeduthz <antonioedu.thz@gmail.com>
2025-09-18 02:45:33 -0300

    fix

--- src/mainJquery.js
+++ src/mainJquery.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(function() {
     const notes = JSON.parse(localStorage.getItem("notes") || "[]")
     let isNoteOn = false
@@ -63,44 +62,4 @@ $(function() {
     }
 
     updateNoteList()
-=======
-$(function() {
-    const notes = JSON.parse(localStorage.getItem("notes") || "[]")
-    let isNoteOn = false
-    let currentNote = 0
-    
-    //Botao para criar uma nova note ou salvar uma já existente
-    $("save-btn").on("click", () => {
-        const title = $("#note-title").val()
-        const content = $("#note-content").val()
-        if (isNoteOn) {
-            notes[currentNote][0] = title
-            notes[currentNote][1] = content
-        } else {
-            notes.push({title: title,content: content})
-        }   
-        localStorage.setItem("notes",JSON.stringify(notes))
-        updateDOM()
-    })
-
-    $("#delete-btn").on("click", () => {
-        if (isNoteOn) {
-            notes.splice(noteIndex,1)
-            localStorage.setItem("notes",JSON.stringify(notes))   
-        }
-        $("")
-    })
-
-    function updateDOM() {
-        const $frag = $()
-        notes.forEach(element => {
-            const $template = $(".note-template").find(".note-preview").clone()
-            
-            $template.find(".title-preview").text(element[0])
-            $template.find(".p-preview").text(element[1])
-            $frag.append(template)
-        });
-        $("#note-main").append($frag)
-    }
->>>>>>> 13d5506b85091bbf4fe7d1410eff92c041c1aaf4
 })
